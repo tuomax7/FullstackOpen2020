a7473c44874cd4292a7ca91355564b1491e603c7
Tuomas Nummela <tuomasnummela@Harri-MacBook-Air.local>
2020-10-25 11:45:39 +0200

    1.5 valmis.

---
+++ Osa1/1.5/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+    	<Header course={course} />
+      	<Content course={course} />
+      	<Total course={course} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+	return(
+		<h1>{props.course.name}</h1>
+	)
+}
+
+const Content = (props) => {
+	return(
+		<div>
+			<Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
+			<Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+			<Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+		</div>
+	)
+}
+
+const Part = (props) => {
+	return(
+		<p>{props.part} {props.exercises}</p>
+	)
+}
+
+const Total = (props) => {
+	return(
+		<p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
