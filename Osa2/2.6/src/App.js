0d7214f1051aafe9bf2e8486b91498a62920b386
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-02-25 14:04:42 +0200

    2.11 done.

--- Osa2/2.6/src/App.js
+++ Osa2/2.6/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
 
   const [ newNumber, setNewNumber ] = useState('')
 
   const [ newFilter, setNewFilter ] = useState('')
+
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const changedFilter = (event) => {
     setNewFilter(event.target.value)
