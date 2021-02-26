fae2fa7f9b9a5ee32234e4632f08d75a51dbce6d
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-02-26 19:21:59 +0200

    2.16 done.

--- Osa2/notes-2.6/src/App.js
+++ Osa2/notes-2.6/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
@@ -14,12 +14,23 @@ const App = () => {
 
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
-      })
+    noteService
+      .getAll()
+        .then(initialNotes => {
+          setNotes(initialNotes)
+        })
   }, [])
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = {...note, important: !note.important};
+
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+          setNotes(notes.map(note => note.id !== id ? note : returnedNote));
+        })
+  }
 
   const addNote = (event) => {
     event.preventDefault();
@@ -30,12 +41,12 @@ const App = () => {
       important: Math.random() > 5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data));
-        setNewNote('');
-      })
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+          setNotes(notes.concat(returnedNote));
+          setNewNote('');
+        })
 
   }
 
@@ -52,10 +63,11 @@ const App = () => {
         </button>
       </div>      
       <ul>
-        {notesToShow.map((note, i) => 
+        {notesToShow.map((note) => 
           <Note
-            key={i}
+            key={note.id}
             note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
           />
         )}
       </ul>
