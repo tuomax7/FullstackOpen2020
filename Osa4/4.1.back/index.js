898ed08c2c81e34359f91d339b57e115d033779b
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2021-03-16 13:10:52 +0200

    4.1 done.

--- Osa4/4.1.back/index.js
+++ Osa4/4.1.back/index.js
@@ -11,6 +11,14 @@ const blogSchema = mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb+srv://bloglistadmin:bloglistadmin@cluster0.qxgn2.mongodb.net/bloglist?retryWrites=true&w=majority'
