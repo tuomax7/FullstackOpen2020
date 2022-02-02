80c861fa9171d3fabbedd1c0cbe0cf41cf97d96c
tuomax7 <tuomas.nummela.per@gmail.com>
2022-02-02 23:33:45 +0200

    4.19 done.

--- Osa4/4.1.back/controllers/blogs.js
+++ Osa4/4.1.back/controllers/blogs.js
@@ -3,6 +3,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+	  return authorization.substring(7)
+	}
+	return null
+  }
+
 blogsRouter.get('/', async (request, response) => {
 
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,8 +20,13 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const users = await User.find({})
-  const user = users[0]
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+	if (!token || !decodedToken.id) {
+	  return response.status(401).json({ error: 'token missing or invalid' })
+	}
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({...request.body, user: user._id})
 
